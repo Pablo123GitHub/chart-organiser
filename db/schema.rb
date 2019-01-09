4480656d4a7122f5505e53150b1ab1ed347b8ef0
Pablo <paul.ladiv123@gmail.com>
2019-01-09 13:27:58 +0000

    scaffolding Category model first

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_08_153544) do
+ActiveRecord::Schema.define(version: 2019_01_09_132424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,10 +36,9 @@ ActiveRecord::Schema.define(version: 2019_01_08_153544) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "charts", force: :cascade do |t|
-    t.string "blindRange"
-    t.string "pokerSituation"
-    t.string "opponentType"
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
