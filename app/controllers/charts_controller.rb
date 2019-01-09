3fbaba1c66aa2ca41bfbbd783797c8699fc46f9f
Pablo <paul.ladiv123@gmail.com>
2019-01-09 15:37:33 +0000

    add ability to delete category and dependent

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -5,6 +5,13 @@ class ChartsController < ApplicationController
         redirect_to category_path(@category)
     end 
 
+    def destroy 
+        @category = Category.find(params[:category_id])
+        @chart = @category.charts.find(params[:id])
+        @chart.destroy
+        redirect_to category_path(@category)
+        end 
+
     private 
     def comment_params
         params.require(:chart).permit(:blindrange, :situation, :opponent )
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-    has_many :charts
+    has_many :charts, dependent: :destroy
     validates :title, presence: true,
                     length: { minimum: 5}
 end
