dae7c61f072b9e3080e808b24055ddf7395c4f97
Pablo <paul.ladiv123@gmail.com>
2019-01-15 15:23:14 +0000

    add error message when image is not uploaded before chart creation

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,9 +2,14 @@ class ChartsController < ApplicationController
     def create 
         @category = Category.find(params[:category_id])
         @chart = @category.charts.create(comment_params)
+        @errors_messages = @chart.errors.messages       
+        full_error_message = ""
+        @errors_messages.each {|key, value| full_error_message = "#{key} " + value[0].to_s}
+         if @errors_messages.any? 
+            flash[:notice] ='ERROR: Chart was not created because ' + full_error_message.to_s
+         end 
         redirect_to category_path(@category)
     end 
-    
 
     def destroy 
         @category = Category.find(params[:category_id])
